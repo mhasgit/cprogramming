960acca0c38edd462aaf10e64d59ec9d6c6b26ba
mhasgit <clumsy7674@gmail.com>
2022-04-11 17:03:36 +0500

    find_power

---
+++ source/Chapter_9/find_power.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+
+int power(int x, int y)
+{
+    int i, result = 1;
+    for (i = 1; i <= y; i++)
+        result = result * x;
+    return result;
+}
+
+int main()
+{
+    int base, exponent;
+    printf("Enter Base Number: ");
+    scanf("%d", &base);
+    printf("Enter Exponent Number: ");
+    scanf("%d", &exponent);
+    
+    printf("The Ans is: %d", power(base, exponent));
+
+    return 0;
+}
