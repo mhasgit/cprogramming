21109e65050aa6ba4aa4da100cc38570901edb20
mhasgit <clumsy7674@gmail.com>
2022-04-12 12:09:29 +0500

    find_prime_no

---
+++ source/Chapter_9/find_prime_no.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdbool.h>
+
+bool prime_number(int n)
+{
+    int divisor;
+    if(n <= 0) 
+    return false;
+    for (divisor = 2; divisor * divisor <= n; divisor++)
+    {
+        if (n % divisor == 0)
+            return false;
+
+        return true;
+    }
+}
+
+int main()
+{
+    int n;
+    printf("Enter any Number: ");
+    scanf("%d", &n);
+
+    if (prime_number(n))
+    printf("You entered a prime number...\n");
+    else
+    printf("Not a prime number...\n");
+
+    return 0;
+}
